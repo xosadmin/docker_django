c37a5725c0ac539a36f4162ecb7f4eca245b80d0
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-08-16 09:31:51 +0800

    Add omitted "."

--- start.sh
+++ start.sh
@@ -9,5 +9,5 @@ pip3 install --no-cache-dir django --break-system-packages
 pip3 install --no-cache-dir -r /app/requirements.txt --break-system-packages
 cd /app
 
-django-admin startproject django_project
+django-admin startproject django_project .
 python3 manage.py runserver
